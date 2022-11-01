0d9dcb178a232e93a14910d41701a1d4cf451af0
Phillip <phillip.lam0@gmail.com>
2022-11-01 12:26:33 -0400

    refactor: abstracted Avatar into own component

---
+++ components/Nav/Avatar.tsx
@@ -0,0 +1,20 @@
+import Image from "next/image";
+
+export default function Avatar() {
+  return (
+    <div className="h-[35px] w-[35px] md:h-[50px] md:w-[50px]">
+      <button
+        className={
+          "hover:border-orange border-transparent border-2 rounded-[30px]"
+        }
+      >
+        <Image
+          src={"/images/image-avatar.png"}
+          alt={"Avatar Image"}
+          width={50}
+          height={50}
+        />
+      </button>
+    </div>
+  );
+}
