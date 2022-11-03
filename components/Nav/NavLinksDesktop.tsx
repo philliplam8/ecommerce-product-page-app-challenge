7d05635b37319f2245ec5a0d61b0b3b27ed85a61
Phillip <phillip.lam0@gmail.com>
2022-11-03 17:23:49 -0400

    fix(nav): decreasing gap on middle breakpoint

--- components/Nav/NavLinksDesktop.tsx
+++ components/Nav/NavLinksDesktop.tsx
@@ -2,7 +2,7 @@ import { NavLink, navLabels } from ".";
 
 export default function NavLinksDesktop() {
   return (
-    <ul className="h-full hidden md:flex flex-row gap-10 items-center bg-white">
+    <ul className="h-full hidden md:flex flex-row md:gap-8 lg:gap-10 items-center bg-white">
       {navLabels.map((item) => {
         return <NavLink key={item} link="/" label={item} />;
       })}
