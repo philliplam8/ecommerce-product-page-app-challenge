5166b8048c47657d3df73a146d67217e74704952
Phillip <phillip.lam0@gmail.com>
2022-11-07 17:18:05 -0500

    fix(nav): replacing ul tags with div tags

--- components/Nav/NavLinksDesktop.tsx
+++ components/Nav/NavLinksDesktop.tsx
@@ -2,10 +2,10 @@ import { NavLink, navLabels } from ".";
 
 export default function NavLinksDesktop(): JSX.Element {
   return (
-    <ul className="h-full hidden md:flex flex-row md:gap-8 lg:gap-10 items-center bg-white">
+    <div className="h-full hidden md:flex flex-row md:gap-8 lg:gap-10 items-center bg-white">
       {navLabels.map((item) => {
         return <NavLink key={item} link="/" label={item} />;
       })}
-    </ul>
+    </div>
   );
 }
