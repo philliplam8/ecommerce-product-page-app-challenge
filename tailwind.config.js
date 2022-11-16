17a1e82486e4856612db63870d60780a82732c45
Phillip <phillip.lam0@gmail.com>
2022-11-16 12:08:29 -0500

    refactor: switching all single quotes to double quotes for consistency

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  darkMode: 'class',
+  darkMode: "class",
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
@@ -21,23 +21,23 @@ module.exports = {
     },
     screens: {
       // Horizontal Media Breakpoints -------------------------------
-      'sm': '640px',
+      "sm": "640px",
       // => @media (min-width: 640px) { ... }
 
-      'md': '768px',
+      "md": "768px",
       // => @media (min-width: 768px) { ... }
 
-      'lg': '1024px',
+      "lg": "1024px",
       // => @media (min-width: 1024px) { ... }
 
-      'xl': '1280px',
+      "xl": "1280px",
       // => @media (min-width: 1280px) { ... }
 
-      '2xl': '1536px',
+      "2xl": "1536px",
       // => @media (min-width: 1536px) { ... }
 
       // Vertical Media Breakpoints for Mobile Landscape ------------
-      'vsm': { 'raw': '(max-height: 800px) and (min-width: 640px)' },
+      "vsm": { "raw": "(max-height: 800px) and (min-width: 640px)" },
       // => @media (max-height: 800px) and (min-width: 640px) { ... }
     },
     extend: {},
